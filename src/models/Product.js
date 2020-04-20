019dcd1dde8f75f4e684ba81cf5af2b58c58b969
Dmytro Portenko <troublesohard@gmail.com>
2020-04-20 16:48:55 +0300

    template refactoring

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,7 +5,6 @@ export class Product {
     this.key = faker.random.uuid();
     this.name = faker.commerce.productName();
     this.price = faker.commerce.price(10, 300);
-    // this.image = faker.random.image();
     this.image = `https://i.picsum.photos/id/${Math.floor((Math.random() * 1000) + 1)}/720/520.jpg`;
     this.description = faker.lorem.paragraphs(3, '\n\n');
   }
